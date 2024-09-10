4db6bd0df4a6086e74beece0c5c11b75fe3ed218
Eric Tsendjav <32915874+sukh0128@users.noreply.github.com>
2024-09-10 22:47:25 +0000

    finished rest of the projects. Done!

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -5,7 +5,26 @@ import PrisonScapeImage2 from '../../public/images/projects/PrisonScape 2.png';
 import TwodPlatformer1 from '../../public/images/projects/2dPlatformer1.png';
 import TwodPlatformer2 from '../../public/images/projects/2dPlatformer2.png';
 import TwodPlatformer3 from '../../public/images/projects/2dPlatformer3.png';
-
+import Mortgage1 from '../../public/images/projects/Mortgage 1.png';
+import Mortgage2 from '../../public/images/projects/Mortgage 2.png';
+import Mortgage3 from '../../public/images/projects/Mortgage 3.png';
+import Hadoop1 from '../../public/images/projects/hadoop1.png';
+import Hadoop2 from '../../public/images/projects/hadoop2.png';
+import Spark1 from '../../public/images/projects/spark1.png';
+import Spark2 from '../../public/images/projects/spark2.png';
+import Spark3 from '../../public/images/projects/spark3.png';
+import Design from '../../public/images/projects/design.png';
+import ChatMNG1 from '../../public/images/projects/chatmng1.png';
+import ChatMNG2 from '../../public/images/projects/chatmng2.png';
+import ChatMNG3 from '../../public/images/projects/chatmng3.png';
+import ChatMNG4 from '../../public/images/projects/chatmng4.png';
+import Retirement1 from '../../public/images/projects/retirement1.png';
+import Retirement2 from '../../public/images/projects/retirement2.png';
+import Retirement3 from '../../public/images/projects/retirement3.png';
+import ML1 from '../../public/images/projects/ML1.png';
+import ML2 from '../../public/images/projects/ML2.png';
+import ML3 from '../../public/images/projects/ML3.png';
+import ML4 from '../../public/images/projects/ML4.png';
 
 const projectList = [
   {
@@ -41,14 +60,16 @@ const projectList = [
   },
   {
     title: "Mortgage Calculator",
-    description: ".NET Core MVC application that helps plan your mortgage.",
+    description: ".NET Core MVC and Razor pages application that helps plan your mortgage. It also has a docker component to it, where you can share the docker image.",
     detailedDescription:
-      "",
+      "A very basic MVC application that uses .NET Core and Razor pages to calculate your mortgage. The application is important to demonstrate the use of .NET Core and Razor pages in a web application. The application also has a docker component to it, where you can share the docker image using docker commands",
     screenshots: [
-
+      Mortgage1,
+      Mortgage2,
+      Mortgage3
     ],
     links: {
-      github: "",
+      github: "https://github.com/sukh0128/MortGauge",
       youtube: ""
     }  
   },
@@ -57,12 +78,17 @@ const projectList = [
     description:
       "Big Data cluster setup using multiple Virtual Machines in Virtual Box using Hadoop and Spark to simulate a real life big data cluster.",
     detailedDescription:
-      "",
+      "The project was to simulate a real life big data cluster using Apache Hadoop and Spark that would be designed for an online retailer system with data centers and servers distributed across multiple cities in different geological locations, such as New York and Tokyo.",
     screenshots: [
-
+      Hadoop1,
+      Hadoop2,
+      Spark1,
+      Spark2,
+      Spark3,
+      Design
     ],
     links: {
-      github: "",
+      github: "https://github.com/sukh0128/CIS602-Final-Project",
       youtube: ""
     }
   },
@@ -71,13 +97,15 @@ const projectList = [
     description:
       "Deployed a machine learning model using Docker and Kubernetes to simulate a real life scenario and demonstrate the integration of containerization and orchestration with Machine Learning models.",
     detailedDescription:
-      "",
+      "The app is a simple flask python app that does the bare minimum, but the main focus of the project was to demonstrate the deployment of a machine learning model using Docker and Kubernetes which serves 3 replicas of the container to account for high availability. The project is important to demonstrate the integration of containerization and orchestration with Machine Learning models.",
     screenshots: [
-
+      ML1,
+      ML2,
+      ML3,
+      ML4
     ],
     links: {
-      github: "",
-      youtube: ""
+      github: "https://github.com/sukh0128/CIS602-Final-Project"
     }  
   },
   {
@@ -85,13 +113,14 @@ const projectList = [
     description:
       "A full stack application that uses .NET Core back-end project and React front-end project to plan for your retirement. Imporant project to demonstrate the integrating separate back-end and front-end projects.",
     detailedDescription:
-      "",
+    "A full stack application that uses .NET Core back-end project and React front-end project to plan for your retirement. Imporant project to demonstrate the integrating separate back-end and front-end projects.",
     screenshots: [
-
+      Retirement1,
+      Retirement2,
+      Retirement3
     ], 
     links: {
-      github: "",
-      youtube: ""
+      github: "https://github.com/sukh0128/RetireEarly"
     }  
   },
   {
@@ -99,13 +128,15 @@ const projectList = [
     description:
       "A full stack application that uses .NET Core back-end project and React front-end project to demonstrate a real-time chat application. The project is important to demonstrate integrating different APIs such as GoogleTranslate and OpenAI in your application.",
     detailedDescription:
-      "",
+      "A full stack application that uses .NET Core back-end project and React front-end project to demonstrate a real-time chat application. The project is important to demonstrate integrating different APIs such as GoogleTranslate and OpenAI in your application.",
     screenshots: [
-
+      ChatMNG1,
+      ChatMNG2,
+      ChatMNG3,
+      ChatMNG4
     ],    
     links: {
-      github: "",
-      youtube: ""
+      github: "https://github.com/sukh0128/chatMNG"
     }  
   },
 ];
