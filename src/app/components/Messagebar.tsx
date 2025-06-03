c478eeea49d70396a9dc9e8f259bdbbc0415b0e3
Eric Tsendjav <erdenesukh@gmail.com>
2025-06-02 21:56:35 -0600

    added cors mode to request

--- src/app/components/Messagebar.tsx
+++ src/app/components/Messagebar.tsx
@@ -30,6 +30,7 @@ const Messagebar = () => {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ question: currMsg }),
+        mode: "cors",
       });
       
       if (!response.ok) {
